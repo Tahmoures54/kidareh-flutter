b0f0a7bbba7bcc98ea037f6bb8ce786724f60e9c
Tahmoures54 <37265001+Tahmoures54@users.noreply.github.com>
2026-09-23 16:58:40 +0330

    feat: bootstrap scalable Flutter app - lib/core/storage/token_storage.dart

---
+++ lib/core/storage/token_storage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class TokenStorage {
+  const TokenStorage();
+
+  static const _key = 'kidareh_session_token';
+  static const _storage = FlutterSecureStorage();
+
+  Future<String?> read() => _storage.read(key: _key);
+  Future<void> write(String token) => _storage.write(key: _key, value: token);
+  Future<void> clear() => _storage.delete(key: _key);
+}
