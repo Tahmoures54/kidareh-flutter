8578efa14ca0e773638c875d2127025f3eb13492
Tahmoures54 <37265001+Tahmoures54@users.noreply.github.com>
2026-09-24 16:27:25 +0330

    انتخاب پیشرفته شهر با چراغ سبز چشمک‌زن

---
+++ lib/core/data/iran_cities.dart
@@ -0,0 +1,43 @@
+class IranCity {
+  const IranCity(this.name, this.province);
+  final String name;
+  final String province;
+}
+
+const iranCities = <IranCity>[
+  IranCity('تهران', 'تهران'),
+  IranCity('کرج', 'تهران'),
+  IranCity('اسلامشهر', 'تهران'),
+  IranCity('ری', 'تهران'),
+  IranCity('پرند', 'تهران'),
+  IranCity('ورامین', 'تهران'),
+  IranCity('قم', 'تهران'),
+  IranCity('مشهد', 'تهران'),
+  IranCity('کرجنو', 'تهران'),
+  IranCity('ملارد', 'تهران'),
+  IranCity('کرجک', 'تهران'),
+  IranCity('شهریار', 'اصفهان'),
+  IranCity('نجف‌آباد', 'اصفهان'),
+  IranCity('کاشان', 'اصفهان'),
+  IranCity('خومینی‌شهر', 'اصفهان'),
+  IranCity('مشهد', 'اصفهان'),
+  IranCity('مشهدرضا', 'اصفهان'),
+  IranCity('كرجان', 'اصفهان'),
+  IranCity('برخوار', 'اصفهان'),
+  IranCity('مشهد', 'اصفهان'),
+  IranCity('تبریز', 'آذربایجان شرقی'),
+  IranCity('مرند', 'آذربایجان شرقی'),
+  IranCity('مراغه', 'آذربایجان شرقی'),
+  IranCity('بناب', 'آذربایجان شرقی'),
+  IranCity('اهر', 'آذربایجان شرقی'),
+  IranCity('ارومیه', 'آذربایجان شرقی'),
+  IranCity('شبستر', 'آذربایجان شرقی'),
+  IranCity('اردبیل', 'آذربایجان شرقی'),
+  IranCity('سراب', 'آذربایجان شرقی'),
+  IranCity('ارومیه', 'آذربایجان شرقی'),
+  IranCity('ارومیه', 'آذربایجان شرقی'),
+  IranCity('ارومیه', 'آذربایجان شرقی'),
+  IranCity('ارومیه', 'آذربایجان شرقی'),
+  IranCity('ارومیه', 'آذربایجان شرقی'),
+  IranCity('ارومیه', 'آذربایجان شرقی'),
+];
