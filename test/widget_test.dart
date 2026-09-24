ec0ac19bdd87216085405df4847dacd026facb6e
Tahmoures54 <37265001+Tahmoures54@users.noreply.github.com>
2026-09-24 13:06:04 +0330

    test(app): wait for initial router restoration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,6 +4,9 @@ import 'package:kidareh_flutter/app/app.dart';
 void main() {
   testWidgets('app starts', (tester) async {
     await tester.pumpWidget(const KidarehApp());
+    await tester.pumpAndSettle();
+
     expect(find.text('کی‌داره؟'), findsOneWidget);
+    expect(find.text('ببین کی داره، حضوری بگیر'), findsOneWidget);
   });
 }
