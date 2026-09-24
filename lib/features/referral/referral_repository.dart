56a2380bc670543b40d9bcbf6eb5e67eae54f7c6
Tahmoures54 <37265001+Tahmoures54@users.noreply.github.com>
2026-09-24 16:04:54 +0330

    حذف ریپازیتوری رفرال از اپ موبایل

--- lib/features/referral/referral_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dio/dio.dart';
-import '../../core/network/api_client.dart';
-class ReferralRepository{
- ReferralRepository({Dio? dio}):_dio=dio??dioProvider.dio;final Dio _dio;
- Future<Map<String,dynamic>> stats()async=>Map<String,dynamic>.from((await _dio.get('/referral/stats')).data);
- Future<List<Map<String,dynamic>>> transactions()async{final d=(await _dio.get('/referral/transactions')).data;return List<Map<String,dynamic>>.from((d['transactions'] as List).map((e)=>Map<String,dynamic>.from(e)));}
-}
